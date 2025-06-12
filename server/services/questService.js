080c1f29be1f669bf63ada7df6d39e3af67e501e
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-12 14:40:35 +0100

    Updated

--- server/services/questService.js
+++ server/services/questService.js
@@ -1,12 +1,12 @@
 import { db } from "../utils/dbConfig.js";
 
-export const createQuest = async (newQuest) => {
+export const createQuest = async (newQuest, userId) => {
   try {
     const { title, description, difficulty, reward_xp } = newQuest;
 
     return await db.query(
-      `INSERT INTO quests (title, description, difficulty, reward_xp ) VALUES($1, $2, $3, $4)RETURNING *`,
-      [title, description, difficulty, reward_xp]
+      `INSERT INTO quests (title, description, difficulty, reward_xp, user_id ) VALUES($1, $2, $3, $4)RETURNING *`,
+      [title, description, difficulty, reward_xp, userId]
     );
   } catch (error) {
     throw error;
