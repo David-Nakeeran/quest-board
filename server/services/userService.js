14af2660b44077213f5a1b7a80b3013415c591b1
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-12 13:16:18 +0100

    Feat: Added user db queries

---
+++ server/services/userService.js
@@ -0,0 +1,22 @@
+import { db } from "./utils/dbConfig.js";
+
+export const createUser = async (newUser, password) => {
+  try {
+    const { email, character_name } = newUser;
+
+    return await db.query(
+      `INSERT INTO users (email, password, character_name) VALUES($1, $2, $3)RETURNING *`,
+      [email, password, character_name]
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserByEmail = async (email) => {
+  try {
+    return await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+  } catch (error) {
+    throw error;
+  }
+};
