992f0f9ee4e4da4c5d86986d974ac8be3157ea0b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-12 15:38:49 +0100

    Chore: Added auth middleware to route

--- server/routes/questRouter.js
+++ server/routes/questRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { questPost } from "../controllers/questController.js";
+import { authenticateToken } from "../auth/auth.js";
 
 const router = express.Router();
 
 // Routes
-router.post("/quests", questPost);
+router.post("/quests", authenticateToken, questPost);
 
 export default router;
