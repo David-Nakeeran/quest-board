92976d93b5ed3e25be93faea0648c84f8ba90f74
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-13 11:32:45 +0100

    Chore: added new route

--- server/routes/questRouter.js
+++ server/routes/questRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { questPost, questAttemptPost } from "../controllers/questController.js";
+import {
+  questPost,
+  questAttemptPost,
+  questGetAvailable,
+} from "../controllers/questController.js";
 import { authenticateToken } from "../auth/auth.js";
 
 const router = express.Router();
 
 // Routes
+router.get("/quests/available", authenticateToken, questGetAvailable);
+
 router.post("/quests", authenticateToken, questPost);
 
 router.post("/quest/:id/complete", authenticateToken, questAttemptPost);
